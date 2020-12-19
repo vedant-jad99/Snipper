aa8c5ce08342b2333918406d17d3ec0e60b21a99
vedant-jad99 <58693619+vedant-jad99@users.noreply.github.com>
2020-12-19 14:14:46 +0530

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+sudo apt-get install python3
+sudo apt-get install python3-pip
+sudo apt-get install python3-tk
+
+pip3 install -r requirements.txt
+
+string='[Desktop Entry]
+Version=1.0
+Type=Application
+Terminal=false
+Name=Snipper\n'
+path_exec="Exec=bash -c $PWD/run.sh
+Icon=/home/thedarkcoder/Desktop/Projects/snipping_tool/assets/Snipper.jpg"
+
+echo "$string$path_exec" > Snipper.desktop
+
+sudo cp Snipper.desktop /usr/share/applications
+
+string_run="#!/bin/sh\n\n"
+path="path=$PWD/main.py\n"
+next_line='/usr/bin/env python3 "$path"'
+
+echo "$string_run$path$next_line" > run.sh
---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+path=/home/thedarkcoder/Desktop/Projects/snipping_tool/main.py
+/usr/bin/env python3 "$path"
