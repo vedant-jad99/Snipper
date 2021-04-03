fcb1ce6209013cf6ed0bba4b778dea72cbc22d01
Vedant <vedantjadhav5566@gmail.com>
2021-04-03 22:55:53 +0530

    Added an uninstall message file in a separate folder

---
+++ uninstall-message/message.py
@@ -0,0 +1,37 @@
+def message():
+    string = """
+********************************************************************************
+********************************************************************************
+    NN    NN    OOOOO    WW     WW     WW     YY    YY   OOOOO    UU     UU
+    NNNN  NN  OO     OO   WW   WWWW   WW       YY  YY  OO     OO  UU     UU
+    NN NN NN  OO     OO    WW  WWWW  WW         YYYY   OO     OO  UU     UU
+    NN  NNNN  OO     OO     WWWW  WWWW           YY    OO     OO  UU     UU
+    NN   NNN    OOOOO        WWW  WWW            YY      OOOOO      UUUUU
+
+    
+    DDDDDD    EEEEEEEE  LLL        EEEEEEEE  TTTTTTTTT  EEEEEEEE
+    DD    DD  EEE       LLL        EEE          TTT     EEE
+    DD    DD  EEEEEEEE  LLL        EEEEEEEE     TTT     EEEEEEEE
+    DD    DD  EEE       LLL        EEE          TTT     EEE
+    DDDDDD    EEEEEEEE  LLLLLLLLL  EEEEEEEE     TTT     EEEEEEEE
+         
+    
+    TTTTTTTTT  HH    HH  II   SSSSSS
+       TTT     HH    HH  II  SSS     
+       TTT     HHHHHHHH  II   SSSSSSS
+       TTT     HH    HH  II        SSS
+       TTT     HH    HH  II    SSSSSS
+       
+    
+    FFFFFFFF    OOOOO    LLL        DDDDDD    EEEEEEEE  RRRRRR
+    FFF       OO     OO  LLL        DD    DD  EEE       RR   RR
+    FFFFFFFF  OO     OO  LLL        DD    DD  EEEEEEEE  RRRRRR 
+    FFF       OO     OO  LLL        DD    DD  EEE       RR   RR
+    FFF         OOOOO    LLLLLLLLL  DDDDDD    EEEEEEEE  RR    RR   
+    
+****************************************************************************************************************************************************************
+    """
+    return string
+
+
+print(message())
--- uninstall.sh
+++ uninstall.sh
@@ -5,3 +5,6 @@ pip3 uninstall -r requirements.txt
 sudo apt autoremove scrot
 
 sudo rm /usr/share/applications/Snipper.desktop
+
+path="$PWD/uninstall-message/message.py"
+python3 $path
